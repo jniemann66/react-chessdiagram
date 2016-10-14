33d4f97548e1f82a17fe3de0b002797e90b63d1d
Judd Niemann <jniemann666@bigpond.com>
2016-10-15 00:44:59 +1100

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import Chessdiagram from './chessdiagram.js';		
 
-
-
-
 import './App.css';
 
-const pieces2=[
+const pieces=[
 			'R@a1', 'N@b1', 'B@c1', 'Q@d1', 'K@e1', 'B@f1', 'N@g1', 'R@h1',
 			'P@a2', 'P@b2', 'P@c2', 'P@d2', 'P@e2', 'P@f2', 'P@g2', 'P@h2',
 			'p@a7', 'p@b7', 'p@c7', 'p@d7', 'p@e7', 'p@f7', 'p@g7', 'p@h7',
@@ -61,8 +58,8 @@ class App extends Component {
 // pieces={pieces2}
 
 /*
-<Chessdiagram flip={false} pieces={pieces2} squareSize={30} lightSquareColor="#2492FF" darkSquareColor="#005EBB" onMovePiece={this._onMovePiece.bind(this)}/>
-				<Chessdiagram  flip={true} pieces={pieces2}  squareSize={30} lightSquareColor="#2492FF" darkSquareColor="#005EBB" onMovePiece={this._onMovePiece.bind(this)}/>
+<Chessdiagram flip={false} pieces={pieces} squareSize={30} lightSquareColor="#2492FF" darkSquareColor="#005EBB" onMovePiece={this._onMovePiece.bind(this)}/>
+				<Chessdiagram  flip={true} pieces={pieces}  squareSize={30} lightSquareColor="#2492FF" darkSquareColor="#005EBB" onMovePiece={this._onMovePiece.bind(this)}/>
 */
 
 export default App;
@@ -71,10 +68,3 @@ export default App;
 
 		// "#2492FF" : "#005EBB"; // blue
 		// "#ff20ff" : "#7f107f" // pink
-
-	/* 	const FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-		const FEN2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
-
-		const pieces = ['suck it','q@C4'];	
-	*/	
-		

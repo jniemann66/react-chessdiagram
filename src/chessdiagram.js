db57ef9b0eae4612e46578d3eec6bd9c0b856c80
jniemann66 <jniemann666@bigpond.com>
2016-11-12 10:25:02 +1100

    clean up indentation etc

--- src/chessdiagram.js
+++ src/chessdiagram.js
@@ -68,8 +68,8 @@ class Chessdiagram extends Component {
 			nextProps.ranks !== this.props.ranks ||
 			nextProps.files !== this.props.files
 		) {
-				let evt = new Event('resize');
-				dispatchEvent(evt); // synthetically trigger resize event. (Can't get coords in here, because component not rendered yet ...)
+			let evt = new Event('resize');
+			dispatchEvent(evt); // synthetically trigger resize event. (Can't get coords in here, because component not rendered yet ...)
 		}
 	}
 
@@ -260,8 +260,8 @@ class Chessdiagram extends Component {
 	}
 
 	_getPieceAtSquare(square) {
-			let pieces = this.props.fen ? this._getPiecesFromFEN() : this._getPieces();
-			return pieces.filter(pieceLocation => pieceLocation.square === square)[0];
+		let pieces = this.props.fen ? this._getPiecesFromFEN() : this._getPieces();
+		return pieces.filter(pieceLocation => pieceLocation.square === square)[0];
 	}
 
 	// render function

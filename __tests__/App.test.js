e2e4f4d465e84169b828a79918e3308705a87708
jniemann66 <jniemann666@bigpond.com>
2016-11-11 16:41:35 +1100

    Added Test for FEN interpretation

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -6,20 +6,52 @@ import sinon from 'sinon';
 
 const startPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
+describe('testing interpretation of FEN string', () => {
+	it('should put pieces on the correct squares', () => {
+
+		const pieces = {
+			'a1': 'R', 'b1': 'N', 'c1': 'B', 'd1': 'Q', 'e1': 'K', 'f1': 'B', 'g1': 'N', 'h1': 'R',
+			'a2': 'P', 'b2': 'P', 'c2': 'P', 'd2': 'P', 'e2': 'P', 'f2': 'P', 'g2': 'P', 'h2': 'P',
+			'a7': 'p', 'b7': 'p', 'c7': 'p', 'd7': 'p', 'e7': 'p', 'f7': 'p', 'g7': 'p', 'h7': 'p',
+			'a8': 'r', 'b8': 'n', 'c8': 'b', 'd8': 'q', 'e8': 'k', 'f8': 'b', 'g8': 'n', 'h8': 'r'
+		}
+
+		const posKeys = Object.keys(pieces);
+
+		const wrapper = mount(
+			<Chessdiagram ref="cd" ranks={8} files={8} fen={startPosition} />
+		);
+
+		let instance = wrapper.instance();
+		console.log(instance._getPieceAtSquare('d4'));
+
+
+		posKeys.map((sq)=>{
+			expect(instance._getPieceAtSquare(sq).pieceType).toBe(pieces[sq]);
+		});
+
+	
+
+		wrapper.unmount();
+
+	});
+});
+
+
+
 describe('When selecting squares at each corner of 8x8 board', () => {
 	it('should return correct names of squares (a1,h1,h8,a8)', () => {
 		let spySelectSquare = sinon.spy();
-		
+
 		const wrapper = mount(
-			<Chessdiagram onSelectSquare={spySelectSquare} ranks={8} files={8}/>
+			<Chessdiagram onSelectSquare={spySelectSquare} ranks={8} files={8} />
 		);
-		
-		console.log(wrapper.state());
+
 		let squareSize = wrapper.props().squareSize;
 		let ranks = wrapper.props().ranks;
 		let files = wrapper.props().files;
 		let halfSquareSize = squareSize / 2; // offset to place coordinate in _middle_ of square
-		
+
 		let a1Coords = {
 			clientX: squareSize + halfSquareSize, // NOTE: labels expected at column 0; board starts at column 1
 			clientY: (ranks - 1) * squareSize + halfSquareSize
@@ -38,8 +70,8 @@ describe('When selecting squares at each corner of 8x8 board', () => {
 		let a8Coords = {
 			clientX: squareSize + halfSquareSize,
 			clientY: halfSquareSize
-		}; 
-	
+		};
+
 		// simulate a click on each corner square
 		wrapper.simulate('mousedown', a1Coords);
 		wrapper.simulate('mousedown', h1Coords);
@@ -59,17 +91,16 @@ describe('When selecting squares at each corner of 8x8 board', () => {
 describe('When moving pawn from e2-e4 on 8x8 board', () => {
 	it('should return piece and names of squares (P,e2,e4)', () => {
 		let spyMovePiece = sinon.spy();
-		
+
 		const wrapper = mount(
-			<Chessdiagram onMovePiece={spyMovePiece} ranks={8} files={8} fen={startPosition}/>
+			<Chessdiagram onMovePiece={spyMovePiece} ranks={8} files={8} fen={startPosition} />
 		);
-		
-		console.log(wrapper.state());
+
 		let squareSize = wrapper.props().squareSize;
 		let ranks = wrapper.props().ranks;
 		let files = wrapper.props().files;
 		let halfSquareSize = squareSize / 2; // offset to place coordinate in _middle_ of square
-		
+
 		let e2Coords = {
 			clientX: 5 * squareSize + halfSquareSize, // NOTE: labels expected at column 0; board starts at column 1
 			clientY: (ranks - 2) * squareSize + halfSquareSize
@@ -79,16 +110,17 @@ describe('When moving pawn from e2-e4 on 8x8 board', () => {
 			clientX: 5 * squareSize + halfSquareSize,
 			clientY: (ranks - 4) * squareSize + halfSquareSize
 		};
-	
+
 		// simulate mousedown on e2
 		wrapper.simulate('mousedown', e2Coords);
 
 		// simulate mouseup on e4
 		wrapper.simulate('mouseup', e4Coords);
 
-		expect(spyMovePiece.calledWith('P','e2','e4')).toBe(true);
+		expect(spyMovePiece.calledWith('P', 'e2', 'e4')).toBe(true);
 
 		wrapper.unmount();
 
 	});
 });
+

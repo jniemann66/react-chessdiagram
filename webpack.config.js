a523f7397bc17771b1dce61a7d7ded4456a3e6f6
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2017-04-01 14:37:01 -0600

    update webpack / sinon
    add externals for enzyme compatability

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  
+
     entry: {
       dev: ['./demo/App.js'], // dev version includes a host application to demonstrate the component
       dist: ['./src/chessdiagram.js'] // dist version is component only
     },
-    output: { 
+    output: {
       path: path.join(__dirname, '/build/'),
       filename: '[name].chessdiagram.js', // will be 'dev.chessdiagram.js' and 'dist.chessdiagram.js'
       publicPath: '/',                     // ./build => http://localhost:port/
       libraryTarget: 'umd'
     },
-  
+
      externals: {
         react: {
             root: 'React',
@@ -26,23 +26,35 @@ module.exports = {
             commonjs2: 'react-dom',
             commonjs: 'react-dom',
             amd: 'react-dom'
-        }
+        },
+        // Externals to allow testing with enzyme
+        'cheerio': 'window',
+        'react/addons': 'react',
+        'react/lib/ExecutionEnvironment': 'react',
+        'react/lib/ReactContext': 'react'
     },
 
     module: {
-        loaders: [
-            { 
-                test: /.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
+      rules: [
+        {
+          test: /.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        },
+        {
+          test: /\.css$/,
+          loaders: [
+            {
+              loader: 'style-loader'
             },
             {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
+              loader: 'css-loader'
             }
-        ]
+          ]
+        }
+      ]
     },
 };
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,31 +2,38 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  
+
     entry: {
       dev: ['./demo/App.js'], // dev version includes a host application to demonstrate the component
       dist: ['./src/chessdiagram.js'] // dist version is component only
     },
-    output: { 
+    output: {
       path: path.join(__dirname, '/build/'),
       filename: '[name].chessdiagram.js', // will be 'dev.chessdiagram.js' and 'dist.chessdiagram.js'
       publicPath: '/',                     // ./build => http://localhost:port/
     },
 
     module: {
-        loaders: [
-            { 
-                test: /.jsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
+      rules: [
+        {
+          test: /.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        },
+        {
+          test: /\.css$/,
+          loaders: [
+            {
+              loader: 'style-loader'
             },
             {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader'
+              loader: 'css-loader'
             }
-        ]
+          ]
+        }
+      ]
     },
 };
